57eadc09f66a358bead7415d45e7ce042a3378dc
chenhh <chenhh@x220>
2014-02-20 18:44:42 +0800

    ptt

--- PySPPortfolio/HeuristicMomentMatch.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-'''
-@author: Hung-Hsin Chen
-@mail: chenhh@par.cse.nsysu.edu.tw
-'''
-
-if __name__ == '__main__':
-    pass
--- PySPPortfolio/MultiStagePortfolio.py
+++ PySPPortfolio/MultiStagePortfolio.py
@@ -4,5 +4,21 @@
 @mail: chenhh@par.cse.nsysu.edu.tw
 '''
 
+def OptimalMultiStagePortfolio(riskyRetMtx, riskFreeRetMtx, 
+                               buyTransFeeMtx, sellTransFeeMtx):
+    '''
+    -假設資料共T期, 投資在M個risky assets, 以及1個riskfree asset
+    -求出每個risky asset每一期應該要買進以及賣出的金額
+    @param riskyRetMtx, numpy.array, size: M * T
+    @param riskFreeRetMtx, numpy.array, size: T
+    @param buyTransFeeMtx, numpy.array, size: M * T
+    @param sellTransFeeMtx, numpy.array, size: M * T
+    
+    @return (buyMtx, sellMtx), numpy.array, each size: M*T
+    '''
+    assert riskyRetMtx.shape == buyTransFeeMtx.shape == sellTransFeeMtx.shape
+    assert riskyRetMtx.shape[1] == riskFreeRetMtx.size
+     
+
 if __name__ == '__main__':
     pass
