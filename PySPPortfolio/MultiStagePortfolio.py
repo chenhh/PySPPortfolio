22bc1234cac828e9dbc6ea8595838f452c0acc83
chenhh <chenhh@x220>
2014-02-21 22:24:21 +0800

    doc

--- PySPPortfolio/MultiStagePortfolio.py
+++ PySPPortfolio/MultiStagePortfolio.py
@@ -4,6 +4,10 @@
 @mail: chenhh@par.cse.nsysu.edu.tw
 '''
 
+from coopr.pyomo import (Set, RangeSet, Param, Var, Objective, Constraint,
+                         AbstractModel, Reals, NonNegativeReals)
+import coopr.opt.SolverFactory as optSolverFactory
+
 def optimalMultiStagePortfolio(riskyRetMtx, riskFreeRetMtx, 
                                buyTransFeeMtx, sellTransFeeMtx):
     '''
@@ -18,8 +22,36 @@ def optimalMultiStagePortfolio(riskyRetMtx, riskFreeRetMtx,
     '''
     assert riskyRetMtx.shape == buyTransFeeMtx.shape == sellTransFeeMtx.shape
     assert riskyRetMtx.shape[1] == riskFreeRetMtx.size
+    
+    opt = optSolverFactory("cplex")
+
+    model = AbstractModel()
+    
+    #number of asset and number of periods
+    model.M = RangeSet(riskyRetMtx.shape[0])
+    model.T = RangeSet(riskyRetMtx.shape[1])
+    
+    #parameter
+    model.riskyRet = Param(model.M, model.T, within=Reals)
+    model.riskfreeRet = Param(model.T, within=Reals)
+    model.buyTransFeeRate = Param(model.M, model.T, within=NonNegativeReals)
+    model.sellTransFeeRate = Param(model.M, model.T, within=NonNegativeReals)
+    model.wealth = Param()
+    
+    def riskyWealth():
+        pass
+    
+    def riskLessWealth():
+        pass
+    
+    #decision variables
+    model.buys = Var(model.M, model.T, within=NonNegativeReals)
+    model.sells = Var(model.M, model.T, within=NonNegativeReals)
      
-     
+    #objective
+    
+    #constraint
+    
 
 if __name__ == '__main__':
     pass
