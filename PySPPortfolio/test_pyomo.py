4611cc392044955d4a5adc6acf75f83d07ff1f34
chenhh <chenhh@x220>
2014-02-22 17:50:53 +0800

    pharmacy

--- PySPPortfolio/test_pyomo.py
+++ PySPPortfolio/test_pyomo.py
@@ -6,8 +6,14 @@ Created on 2014/2/21
 
 from __future__ import division
 from coopr.pyomo import *
+from coopr.opt import SolverFactory
 
-def Pharmacy():
+def abstractPharmacy():
+    # Create a solver
+    
+    opt = SolverFactory('cplex')
+    
+    #build model
     model = AbstractModel()
     
     #set
@@ -30,35 +36,49 @@ def Pharmacy():
     model.D = Var(model.drug, domain=NonNegativeReals)
     model.R = Var(model.material, domain=NonNegativeReals)
      
-    
     #objective
     def objective_rule(model):
-        return summation(model.sellPrice, model.D) -summation(model.EQCost, model.D) - summation(model.materialCost, model.R) 
+        return summation(model.sellPrice, model.D) - \
+               summation(model.EQCost, model.D) - \
+               summation(model.materialCost, model.R) 
     
-    model.OBJ = Objective(rule=objective_rule, sense=maximize)
+    model.Objective = Objective(rule=objective_rule, sense=maximize)
     
     #constraint
     def balance_constraint_rule(model):
-        return summation(model.materialContent, model.R) - (model.grams, model.D) >=0
+        return summation(model.materialContent, model.R) - \
+            summation(model.grams, model.D) >= 0
     
     def storage_constraint_rule(model):
         return summation(model.R) <= model.budgeValue['storage']
     
     def HR_constraint_rule(model):
-        return summation(model.HRhour, model.D) <= model.budgeValue['HR']
+        return summation(model.HRhours, model.D) <= model.budgeValue['HR']
         
     def EQ_constraint_rule(model):
-        return summation(model.EQhour, model.D) <= model.budgeValue['hour']
+        return summation(model.EQhours, model.D) <= model.budgeValue['hour']
     
     def money_constraint_rule(model):
-        return summation(model.EQCost, model.D) + summation(model.materialCost, model.R) <=model.budgeValue['money']
+        return (summation(model.EQCost, model.D) + 
+                summation(model.materialCost, model.R)) <=model.budgeValue['money']
     
-        
     model.balanceConstraint = Constraint(rule=balance_constraint_rule)
     model.storageConstraint = Constraint(rule=storage_constraint_rule)
     model.HRConstraint = Constraint(rule=HR_constraint_rule)
     model.EQConstraint = Constraint(rule=EQ_constraint_rule)
     model.moneyConstraint = Constraint(rule=money_constraint_rule)
 
+    # Create a model instance and optimize
+    data = DataPortal()
+    data.load(filename='pharmacy.dat')
+    instance = model.create(data)
+#     model.pprint()
+    results = opt.solve(instance)
+    print results
+
+def concretePharmacy():
+    model = ConcreteModel()
+    
+
 if __name__ == '__main__':
-    pass
+    abstractPharmacy()
