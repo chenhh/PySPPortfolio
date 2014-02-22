d6db86cdfc32d3a663a5a104003602bc805123b9
chenhh <chenhh@x220>
2014-02-22 22:29:44 +0800

    doc

--- PySPPortfolio/MultiStagePortfolio.py
+++ PySPPortfolio/MultiStagePortfolio.py
@@ -3,16 +3,17 @@
 @author: Hung-Hsin Chen
 @mail: chenhh@par.cse.nsysu.edu.tw
 '''
-
+import time
 from coopr.pyomo import (Set, RangeSet, Param, Var, Objective, Constraint,
-                         AbstractModel, Reals, NonNegativeReals)
-import coopr.opt.SolverFactory as optSolverFactory
+                         ConcreteModel, Reals, NonNegativeReals)
+fromt coopr.opt import  SolverFactory
 
-def optimalMultiStagePortfolio(riskyRetMtx, riskFreeRetMtx, 
+def optimalMultiStagePortfolio(symbols, riskyRetMtx, riskFreeRetMtx, 
                                buyTransFeeMtx, sellTransFeeMtx):
     '''
     -假設資料共T期, 投資在M個risky assets, 以及1個riskfree asset
     -求出每個risky asset每一期應該要買進以及賣出的金額
+    @param symbols, list of string, 
     @param riskyRetMtx, numpy.array, size: M * T
     @param riskFreeRetMtx, numpy.array, size: T
     @param buyTransFeeMtx, numpy.array, size: M * T
@@ -22,35 +23,71 @@ def optimalMultiStagePortfolio(riskyRetMtx, riskFreeRetMtx,
     '''
     assert riskyRetMtx.shape == buyTransFeeMtx.shape == sellTransFeeMtx.shape
     assert riskyRetMtx.shape[1] == riskFreeRetMtx.size
-    
-    opt = optSolverFactory("cplex")
 
-    model = AbstractModel()
+    t1 = time.time()
+    #create model
+    model = ConcreteModel()
     
     #number of asset and number of periods
-    model.M = RangeSet(riskyRetMtx.shape[0])
+    model.M = Set(symbols)
     model.T = RangeSet(riskyRetMtx.shape[1])
+    model.Tp1 = RangeSet(riskyRetMtx.shape[1]+1)
     
     #parameter
-    model.riskyRet = Param(model.M, model.T, within=Reals)
-    model.riskfreeRet = Param(model.T, within=Reals)
-    model.buyTransFeeRate = Param(model.M, model.T, within=NonNegativeReals)
-    model.sellTransFeeRate = Param(model.M, model.T, within=NonNegativeReals)
-    model.wealth = Param()
-    
-    def riskyWealth():
+    def riskyRet_init(model, m, t):
         pass
     
-    def riskLessWealth():
+    def riskfreeRet_init(model, t):
         pass
+    
+    def buyTransFee_init(model, m, t):
+        pass
+    
+    def sellTransFee_init(model, m, t):
+        pass
+    
+    model.riskyRet = Param(model.M, model.Tp1, within=Reals, initialize=riskyRet_init)
+    model.riskfreeRet = Param(model.Tp1, within=Reals, initilize=riskfreeRet_init)
+    model.buyTransFee = Param(model.M, model.T, within=NonNegativeReals, initialize=buyTransFee_init)
+    model.sellTransFee = Param(model.M, model.T, within=NonNegativeReals, initialize=sellTransFee_init)
+    
     
     #decision variables
     model.buys = Var(model.M, model.T, within=NonNegativeReals)
     model.sells = Var(model.M, model.T, within=NonNegativeReals)
+    model.wealth = Var(model.T, within=NonNegativeReals)
      
     #objective
+    def objective_rule(model, t):
+        pass
+    
+    model.objective = Objective(rule=objective_rule, sense=maximize)
     
     #constraint
+    def riskyWealth_constraint_rule(model, m, t):
+        pass
+    
+    def riskyfreeWealth_constraint_rule(model, m, t):
+        pass
+    
+    model.riskyWeathConstraint = Constraint(rule=riskyWealth_constraint_rule)
+    model.riskfreeConstraint = Constraint(rule=riskyfreeWealth_constraint_rule)
+    
+    #optimizer
+    opt = SolverFactory('cplex')
+    opt.options["threads"] = 4
+    
+    instance = model.create()
+    results = opt.solve(instance)
+    print results
+    
+    instance.load(results)
+    for var in instance.active_components(Var):
+        varobj = getattr(instance, var)
+        for idx in varobj:
+            print varobj[idx], varobj[idx].value
+   
+    print "elapsed %.3f secs"%(time.time()-t1)
     
 
 if __name__ == '__main__':
--- PySPPortfolio/test_pyomo.py
+++ PySPPortfolio/test_pyomo.py
@@ -7,12 +7,11 @@ Created on 2014/2/21
 from __future__ import division
 from coopr.pyomo import *
 from coopr.opt import SolverFactory
+import time
 
 def abstractPharmacy():
-    # Create a solver
-    
-    opt = SolverFactory('cplex')
-    
+    t1 = time.time()
+
     #build model
     model = AbstractModel()
     
@@ -28,7 +27,7 @@ def abstractPharmacy():
     model.EQhours = Param(model.drug)
     model.EQCost = Param(model.drug)
     
-    model.materialCost = Param(model.material)
+    model.materialCost = Param(model.material, mutable=True)
     model.materialContent = Param(model.material)
     model.budgeValue = Param(model.budget)
     
@@ -69,16 +68,134 @@ def abstractPharmacy():
     model.moneyConstraint = Constraint(rule=money_constraint_rule)
 
     # Create a model instance and optimize
+    # Create a solver
+    opt = SolverFactory('cplex')
+    
     data = DataPortal()
     data.load(filename='pharmacy.dat')
     instance = model.create(data)
-#     model.pprint()
+    instance.pprint()
     results = opt.solve(instance)
     print results
-
+    print "original elapsed %.3f secs"%(time.time()-t1)
+    
+    t2 = time.time()
+    #change parameter and resolve
+    getattr(instance, "materialCost")[2] = 199
+    instance.preprocess()
+    results = opt.solve(instance)
+    print results
+ 
+ 
+    print "resolve, elapsed %.3f secs"%(time.time()-t2)
+    
+    
+    
 def concretePharmacy():
+    t1 = time.time()
+    
     model = ConcreteModel()
     
-
+    model.drug=[1,2]
+    model.material = [1,2]
+    model.budget = ["money", "HR", "hour", "storage"]
+    #set
+#     model.drug = Set(initialize=[1,2])
+#     model.material = Set(initialize=[1,2])
+#     model.budget = Set(initialize=["money", "HR", "hour", "storage"])
+    
+    #parameter
+#     model.sellPrice = Param(model.drug, initialize={1:6200, 2:6900})
+    model.sellPrice = Param(model.drug, mutable=True)
+    model.sellPrice[1] = 6200
+    model.sellPrice[2] = 6900
+    model.grams = Param(model.drug, initialize={1:0.5, 2:0.6})
+    model.HRhours = Param(model.drug, initialize={1:90, 2:100})
+    model.EQhours = Param(model.drug, initialize={1:40, 2:50})
+    model.EQCost = Param(model.drug, initialize={1:700, 2:800})
+    
+    model.materialCost = Param(model.material, initialize={1:100, 2:199.9},
+                               mutable=True)
+    model.materialContent = Param(model.material, initialize={1:0.01, 2:0.02})
+    model.budgeValue = Param(model.budget, initialize={"money":1e5, "HR":2000, 
+                                            "hour":800, "storage":1000})
+    
+    #decision variables
+    model.D = Var(model.drug, domain=NonNegativeReals)
+    model.R = Var(model.material, domain=NonNegativeReals)
+     
+    #objective
+    def objective_rule(model):
+        return summation(model.sellPrice, model.D) - \
+               summation(model.EQCost, model.D) - \
+               summation(model.materialCost, model.R) 
+    
+    model.Obj = Objective(rule=objective_rule, sense=maximize)
+    
+    #constraint
+    def balance_constraint_rule(model):
+        return summation(model.materialContent, model.R) - \
+            summation(model.grams, model.D) >= 0
+    
+    def storage_constraint_rule(model):
+        return summation(model.R) <= model.budgeValue['storage']
+    
+    def HR_constraint_rule(model):
+        return summation(model.HRhours, model.D) <= model.budgeValue['HR']
+        
+    def EQ_constraint_rule(model):
+        return summation(model.EQhours, model.D) <= model.budgeValue['hour']
+    
+    def money_constraint_rule(model):
+        return (summation(model.EQCost, model.D) + 
+                summation(model.materialCost, model.R)) <=model.budgeValue['money']
+    
+    model.balanceConstraint = Constraint(rule=balance_constraint_rule)
+    model.storageConstraint = Constraint(rule=storage_constraint_rule)
+    model.HRConstraint = Constraint(rule=HR_constraint_rule)
+    model.EQConstraint = Constraint(rule=EQ_constraint_rule)
+    model.moneyConstraint = Constraint(rule=money_constraint_rule)
+    
+    # Create a solver
+    opt = SolverFactory('cplex')
+    opt.options["threads"] = 4
+    instance = model.create()
+    results = opt.solve(instance)
+    print results
+#     print type(results)
+#     print dir(results)
+    
+    instance.load(results)
+    
+    #print variable method 1
+#     print instance.D[1].value
+#     print instance.D[2].value
+#     print instance.R[1].value
+#     print instance.R[2].value
+#     print instance.Obj.value
+    
+    #print variable method 2
+    for var in instance.active_components(Var):
+        varobj = getattr(instance, var)
+        for idx in varobj:
+            print varobj[idx], varobj[idx].value
+    
+  
+    #output file to yaml format
+#     results.write()
+    print "original elapsed %.3f secs"%(time.time()-t1)
+    
+#     t2 = time.time()
+#     #change parameter and resolve
+#     getattr(instance, "materialCost")[2] = 199
+#     instance.preprocess()
+#     results = opt.solve(instance, tee=True)
+#     print results
+#     print "solver status:", str(results.Solution.Status)
+#   
+#  
+#     print "resolve, elapsed %.3f secs"%(time.time()-t2)
+    
 if __name__ == '__main__':
-    abstractPharmacy()
+#     abstractPharmacy()
+    concretePharmacy()
