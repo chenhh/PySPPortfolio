01d2417ada46bb880cce4ee7b13cec08f03bb999
chenhh <chen1116@gmail.com>
2014-03-03 15:22:40 +0800

    cvar

--- PySPPortfolio/example/pysp_portfolio_minCVaR/ReferenceModel.py
+++ PySPPortfolio/example/pysp_portfolio_minCVaR/ReferenceModel.py
@@ -3,7 +3,6 @@
 @author: Hung-Hsin Chen
 @mail: chenhh@par.cse.nsysu.edu.tw
 minimize the CVaR
-
 '''
 
 from coopr.pyomo import *
@@ -17,23 +16,31 @@ model.symbols = Set()
 model.riskyRet = Param(model.symbols)
 model.riskFreeRet = Param()
 
-#return of next period, uncertain
-model.predictRiskyRet = Param(model.symbols) 
-model.predictRiskFreeRet = Param()
-
 model.allocatedWealth = Param(model.symbols)
 model.depositWealth = Param()
 
 model.buyTransFee = Param(model.symbols)
 model.sellTransFee = Param(model.symbols)
+model.alpha = Param(within=NonNegativeReals)
+
+#return of next period, uncertain
+model.predictRiskyRet = Param(model.symbols) 
+model.predictRiskFreeRet = Param()
+
 
 #decision variables
 model.buys = Var(model.symbols, within=NonNegativeReals)        #stage 1
 model.sells = Var(model.symbols, within=NonNegativeReals)       #stage 1
 model.riskyWealth = Var(model.symbols, within=NonNegativeReals) #stage 2
 model.riskFreeWealth = Var(within=NonNegativeReals)             #stage 2
-model.Z = Var() #aux variable
-model.Ys = Var(within=NonNegativeReals)
+
+#aux variable, variable in definition of CVaR, equals to VaR at opt. sol.
+model.Z = Var()
+
+#aux variable, portfolio wealth smaller (<=) than VaR
+model.Ys = Var(within=NonNegativeReals)                 
+
+#stage specific variable
 model.FirstStageCost = Var()
 model.SecondStageCost = Var()
 
@@ -41,6 +48,7 @@ model.SecondStageCost = Var()
 def riskyWeathConstraint_rule(model, m):
     '''
     riskyWealth is a decision variable depending on both buys and sells.
+    (it means riskyWealth depending on scenario).
     therefore 
     buys and sells are fist stage variable,
     riskywealth is second stage variable
@@ -70,26 +78,25 @@ def riskFreeWealthConstraint_rule(model):
 model.riskFreeWealthConstraint = Constraint()
 
 def CVaRConstraint_rule(model):
+    '''auxiliary variable Y depends on scenario. CVaR <= VaR
+    '''
     wealth = sum( (1. + model.predictRiskyRet[m] ) * model.riskyWealth[m] 
                  for m in model.symbols)
-    return model.Ys >= wealth
+    return model.Ys >= (model.Z - wealth)
 
 model.CVaRConstraint = Constraint()
 
-def CVaRConstraint1_rule(model):
-
-# Stage-specific 
+# Stage-specific constraints 
 def ComputeFirstStageCost_rule(model):
-    return model.FirstStageCost  == 0.0
+    return model.FirstStageCost  == model.Z
 
 model.ComputeFirstStageCost = Constraint()
 
 def ComputeSecondStageCost_rule(model):
     '''CVaR time (t+1) '''
-    
-    return model.SecondStageCost  == model.Z - 1/(1-.95)* model.Ys
+    return model.SecondStageCost  == -1/(1-model.alpha)* model.Ys
 
-  model.ComputeSecondStageCost = Constraint()
+model.ComputeSecondStageCost = Constraint()
 
 #objective
 def TotalCostObjective_rule(model):
