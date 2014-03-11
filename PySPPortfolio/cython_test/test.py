f43e3623746ec29e4f7a02f4a8f0499874f52bb7
Hung-Hsin Chen <chen1116@gmail.com>
2014-03-11 22:21:31 +0800

    cython

---
+++ PySPPortfolio/cython_test/test.py
@@ -0,0 +1,41 @@
+'''
+Created on 2014/3/11
+
+@author: chenhh
+'''
+from hello import say_hello_to, f, f2
+import time
+
+def fpy(x):
+    return x**2-x
+
+
+def testf():
+    t = time.time()
+    for i in xrange(1000000):
+        fpy(i)
+    print time.time()-t
+    
+    t= time.time()
+    for i in xrange(1000000):
+        f(i)
+    print time.time()-t
+    
+    t= time.time()
+    for i in xrange(1000000):
+        f2(i)
+    print time.time()-t
+
+def test2():
+    '''如果pyx模組不需額外的clib時，可用pyximport直接引入
+    '''
+    import pyximport; pyximport.install()
+    import hello2
+    hello2.hello2("chenhh, hello2")
+
+if __name__ == '__main__':
+    say_hello_to("Chenhh")
+    print f(10)
+    testf()
+    test2()
+    
