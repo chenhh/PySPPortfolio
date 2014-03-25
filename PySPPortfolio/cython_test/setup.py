ab66f8b8ad0558451c188cfdd81b160964cfc7b7
chenhh <chenhh@x220>
2014-03-25 19:23:41 +0800

    tst

--- PySPPortfolio/cython_test/setup.py
+++ PySPPortfolio/cython_test/setup.py
@@ -11,5 +11,5 @@ from Cython.Build import cythonize
 
 setup(
   name = 'Hello world app',
-  ext_modules = cythonize("hello.pyx"),
+  ext_modules = cythonize("*.pyx",),
 ) 
--- PySPPortfolio/cython_test/test.py
+++ PySPPortfolio/cython_test/test.py
@@ -3,7 +3,7 @@ Created on 2014/3/11
 
 @author: chenhh
 '''
-from hello import say_hello_to, f
+from hello import say_hello_to, f, f2
 import time
 
 def fpy(x):
@@ -21,18 +21,14 @@ def testf():
         f(i)
     print time.time()-t
     
-#     t= time.time()
-#     for i in xrange(1000000):
-#         f2(i)
-#     print time.time()-t
-
-def test2():
-    import hello2
-    hello2.hello2("chenhh, hello2")
+    t= time.time()
+    for i in xrange(1000000):
+        f2(i)
+    print time.time()-t
 
 if __name__ == '__main__':
     say_hello_to("Chenhh")
     print f(10)
     testf()
-#     test2()
+
     
