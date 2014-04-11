1e05f549011548b7d62883dd042436573acd6786
chenhh <chenhh@x220>
2014-04-11 22:38:32 +0800

    sparse

--- PySPPortfolio/scenario/Copula.py
+++ PySPPortfolio/scenario/Copula.py
@@ -8,14 +8,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
-def HeuristicCopula(data, alpha=0.95, n_scenario=200):
+
+
+def HeuristicCopula(data, alpha=0.95, n_scenario=200, K=100):
     '''
     pure python version
     @data, numpy.array, size: N*D, N is number of data, D is the dimensions
     @alpha, float, confidence level of CVaR (0.5 <=alpha <=1)
     @n_scenario, integer
+    @K, integer, num. of partitions of [0,1]
     '''
     t0 = time.time()
+    N, D = data.shape
+
     #step 1. computing copula function
     print "Z:", data
     
@@ -26,13 +31,16 @@ def HeuristicCopula(data, alpha=0.95, n_scenario=200):
     empData = rank/data.shape[0]
     print "U:", empData
     
-    #將[0,1]分成K段
-    K = 10
-    delta = np.linspace(0, 1, K)
+    #translate U to subcube index
+    empIdx = np.maximum(np.ceil(empData*K), 1)
+    print "subcube idx:", empIdx
+   
+    #computing f^(d)
+    delta = 1./K
+    inc = 1./N/delta**(D)
+    f = {}
     
-    #scatter plot
-    plt.scatter(empData[:,0], empData[:,1])
-    plt.show()
+    
     
     print "HeuristicCopula_alpha-%s_scen-%s OK, %.3f secs"%(
                     alpha, n_scenario, time.time()-t0)
@@ -59,12 +67,45 @@ def CubicScatter():
     plt.show()
     
 def testHeuristicCopula():
-    n_rv = 50
-    data = np.random.randn(n_rv, 2)
+    n_rv = 10
+    data = np.random.randn(n_rv, 40)
     alpha=0.95
     n_scenario=200
-    HeuristicCopula(data, alpha, n_scenario)
+    HeuristicCopula(data, alpha, n_scenario, K=100)
+
+
+def SparseTree(arr, f, d, D):
+    '''
+    d: current dimension
+    D: maximum dimension
+    '''
+    if d == D:
+        return
+    else:
+        print "d:",d
+        branch = f.setdefault(arr[d], {})
+        if d >= 1:
+            branch['val'] = 0.01 
+            
+        print "f:", f, id(f)
+        print "branch:", branch, id(branch)
+        SparseTree(arr, branch, d+1, D)
+#         print "f:", f
+        return f
+    
+
 
 if __name__ == '__main__':
 #     testHeuristicCopula()
-    CubicScatter()
+#     CubicScatter()
+    
+    arr = np.array([1,3,5,7])
+    d = 0
+    D = 4
+    f = SparseTree(arr, {}, d, D)
+    print
+    print f
+
+    f = SparseTree(np.array([1,2,5,7]), f, d, D)
+    print 
+    print f
