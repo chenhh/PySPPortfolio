081b250f2b6e81f897f01c0195ef137887116dc5
chenhh <chenhh@x220>
2014-04-12 21:32:27 +0800

    copula

--- PySPPortfolio/scenario/Copula.py
+++ PySPPortfolio/scenario/Copula.py
@@ -4,12 +4,13 @@
 @mail: chenhh@par.cse.nsysu.edu.tw
 '''
 from __future__ import division
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 import time
 
 
-def HeuristicCopula(data, alpha=0.95, n_scenario=200, K=100):
+def HeuristicCopula(data, alpha=0.95, n_scenario=200, K=10):
     '''
     pure python version
     @data, numpy.array, size: N*D, N is number of data, D is the dimensions
@@ -31,7 +32,7 @@ def HeuristicCopula(data, alpha=0.95, n_scenario=200, K=100):
     print "U:", empData
     
     #translate U to subcube index
-    empIdx = np.maximum(np.ceil(empData*K), 1)
+    empIdx = np.ceil(empData*K).astype(np.int)
     print "subcube idx:", empIdx
    
     #computing f^(d)
@@ -40,15 +41,44 @@ def HeuristicCopula(data, alpha=0.95, n_scenario=200, K=100):
     f = {}
     for arr in empIdx:
         f= SparseArray(arr, f, 0, D, inc)
-    print "fkey:", f.keys()
+    print "f1key:", f.keys()
+    for key in f.keys():
+        print "f2 keys:", f[key].keys()
     
     #generating samples
     for scen in xrange(n_scenario):
         u = np.random.rand()
-        u1 = np.random.rand()
-        down_u2_idx = down_u2(u, u1, K, D, f)
-        u2 = down_u2_idx * delta 
         
+        i1keys = f.keys()
+        #sampling until i1 in f
+        while True:
+            u1 = np.random.rand()
+            i1 = int(math.ceil(u1*K))
+            if i1 in i1keys:
+                break
+
+        #sampling u2
+        lowerBound = u * K**(D-1) 
+        down_u2, i2 = -1, -1
+        sum_f2, f2 = 0, 0
+        i2keys = f[i1].keys()
+        
+        for kdx in xrange(1, K):
+            if kdx in i2keys:
+                sum_f2 += f[i1][kdx]['val']
+                
+            if sum_f2 >=lowerBound:
+                sum_f2  -= f[i1][kdx]['val']
+                down_u2 = kdx - 1
+                i2 = kdx
+                if i2 in i2keys:
+                    f2 = f[i1][i2]['val']
+        
+        if down_u2 == -1 and i2 == -1:
+            down_u2, i2 = K-1, K
+            f2 = f[i1][i2]['val']
+            
+        u2 = down_u2 * delta + (u-delta**(D-1)*sum_f2)/(delta**(D-2)*f2)
         
         if D > 2:
             pass
@@ -59,24 +89,22 @@ def HeuristicCopula(data, alpha=0.95, n_scenario=200, K=100):
                     alpha, n_scenario, time.time()-t0)
     
 
+def get_down_ud(ud_arr, f, u, K):
+    lowerBound = u * getSparseArrayValue(ud_arr, f)
+    sum_f, down_ud = -1, -1
+    
+    for k in xrange(K):
+        if getSumSparseArrayValue(arr, f, k) >= lowerBound:
+            sum_f = getSumSparseArrayValue(arr, f, k-1)
+            down_ud = k-1
+    
+    if down_ud == -1:
+        down_ud = K
+        sum_f =  getSumSparseArrayValue(arr, f, K)
+        
+    return down_ud, sum_f
 
-def down_u2(u, u1, K, D, f):
-    lowBound = K**(D-1) * u
-    i1 = int(np.maximum(1, np.ceil(u1*K)))
-    fsum, best_u2 = 0, 0
-    for val2 in xrange(K):
-        i2 = int(val2)
-        if i2 in f[i1].keys():
-            fsum += f[i1][i2]['val']
-        
-        if fsum >= lowBound:
-            best_u2 = i2 -1
-            
-    return best_u2
-        
-    
-    
-    return best
+
 
 def CubicScatter():
     from numpy.random import random
@@ -115,7 +143,7 @@ def SparseArray(arr, f, d, D, inc):
     if d == D:
         return
     else:
-        branch = f.setdefault(int(arr[d]), {})
+        branch = f.setdefault(arr[d], {})
         if d >= 1:
             if 'val' in branch.keys():
                 branch['val'] += inc
@@ -125,19 +153,42 @@ def SparseArray(arr, f, d, D, inc):
         SparseArray(arr, branch, d+1, D, inc)
         return f
     
+def getSparseArrayValue(arr, f, d=0):
+    '''
+    given array of first tree node, 
+    to get the value of it in the f
+    '''
+    if d == len(arr):
+        return f['val']
+    else: 
+        if arr[d] in f.keys():
+            return getSparseArrayValue(arr, f[arr[d]], d+1)
+        else:
+            raise ValueError('%dth element of arr is %s, not in f'%(d, arr[d]))
 
+
+def getSumSparseArrayValue(arr, f, upperID, d=0):
+    if d == len(arr):
+        return sum(f[key]['val'] for key in f.keys() if key <= upperID)
+    else:
+        if arr[d] in f.keys():
+            return getSumSparseArrayValue(arr, f[arr[d]], upperID, d+1)
+        else:
+            raise ValueError('%dth element of arr is %s, not in f'%(d, arr[d]))
 
 if __name__ == '__main__':
-    testHeuristicCopula()
+#     testHeuristicCopula()
 #     CubicScatter()
     
-#     arr = np.array([1,3,5,7,9])
-#     d = 0
-#     D = 4
-#     inc = 10
-#     f = SparseArray(arr, {}, d, D, inc)
-#     print "run1"
-#     print f
+    arr = np.array([1,3,5,7,9])
+    d = 0
+    D = 4
+    inc = 10
+    f = SparseArray(arr, {}, d, D, inc)
+    print "run1"
+    print f
+    print getSparseArrayValue(np.array([1,3]), f)
+    print getSumSparseArrayValue(np.array([1,3]), f, 5)
 # 
 #     f = SparseArray(np.array([1,3,5,8, 9]), f, d, D, inc)
 #     print "run2"
