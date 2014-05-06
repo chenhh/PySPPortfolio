c7a14fe653d229ef02c268d078075a7aac32206b
chenhh <chen1116@gmail.com>
2014-05-07 00:06:56 +0800

    cython moment dev

--- PySPPortfolio/scenario/CMoment.pyx
+++ PySPPortfolio/scenario/CMoment.pyx
@@ -92,27 +92,70 @@ cpdef HeuristicMomentMatching (np.ndarray[DTYPE_t, ndim=2]  tgtMoms,
 
 
 
-
-
-cdef np.ndarray Cholesky (np.ndarray tgtMoms, np.ndarray tgtCorrs):
-    pass
-
-cdef np.ndarray MomentCorrStats (np.ndarray outMoms, np.ndarray outCorrs,
-                                 np.ndarray tgtMoms, np.ndarray tgtCorrs):
-    pass
-
-
-def RMSE(np.ndarray[DTYPE_t] srcArr, np.ndarray[DTYPE_t] tgtArr):
+def cubicFunction(cubParams, sampleMoms, tgtMoms):
     '''
-    srcArr, numpy.array
-    tgtArr, numpy.array
+    cubParams: (a,b,c,d)
+    EY: 4 moments of target
+    EX: 12 moments of samples
+    '''
+    a, b, c, d = cubParams
+    EX = sampleMoms
+    EY = tgtMoms
+    
+    v1 = a + b*EX[0] + c*EX[1] + d*EX[2] - EY[0]
+    
+    v2 = ((d*d)*EX[5] + 2*c*d*EX[4] + (2*b*d + c*c)*EX[3] + 
+          (2*a*d + 2*b*c)*EX[2] + (2*a*c + b*b)*EX[1] + 2*a*b*EX[0] + a*a - EY[1])
+    
+    v3 = ((d*d*d)*EX[8] + (3*c*d*d)*EX[7] + (3*b*d*d + 3*c*c*d)*EX[6] + 
+          (3*a*d*d + 6*b*c*d + c*c*c)*EX[5] + (6*a*c*d + 3*b*b*d + 3*b*c*c)*EX[4] + 
+          (a*(6*b*d + 3*c*c) + 3*b*b*c)*EX[3] + (3*a*a*d + 6*a*b*c + b*b*b) *EX[2]+ 
+          (3*a*a*c + 3*a*b*b)*EX[1] + 3*a*a*b*EX[0] + a*a*a - EY[2])
+    
+    v4 = ((d*d*d*d)*EX[11] + (4*c*d*d*d)*EX[10] + (4*b*d*d*d + 6*c*c*d*d)*EX[9] + 
+          (4*a*d*d*d + 12 *b*c*d*d + 4*c*c*c*d)*EX[8] + 
+          (12*a*c*d*d + 6*b*b*d*d + 12*b*c*c*d + c*c*c*c)*EX[7] + 
+          (a*(12*b*d*d + 12*c*c*d) + 12*b*b*c*d + 4*b*c*c*c)*EX[6] + 
+          (6*a*a*d*d+a*(24*b*c*d+4*c*c*c)+4*b*b*b*d+6*b*b*c*c) *EX[5] + 
+          (12*a*a*c*d+ a*(12*b*b*d+12*b*c*c)+4*b*b*b*c)*EX[4] + 
+          (a*a*(12*b*d+6*c*c)+12*a*b*b*c+b*b*b*b)*EX[3] + 
+          (4*a*a*a*d + 12*a*a*b*c+4*a*b*b*b)*EX[2] + 
+          (4*a*a*a*c+6*a*a*b*b)*EX[1] + 
+          (4*a*a*a*b)*EX[0] + a*a*a*a - EY[3])
+    
+    return v1, v2, v3, v4
+
+
+cpdef errorStatistics(np.ndarray[DTYPE_t, ndim=2] outMtx, 
+                    np.ndarray[DTYPE_t, ndim=2] tgtMoms, 
+                    np.ndarray[DTYPE_t, ndim=2] tgtCorrs):
+                    
+    cdef: 
+        int n_rv = outMtx.shape[0]
+        np.ndarray[DTYPE_t, ndim=2] outMoms = np.empty((n_rv, 4))
+        np.ndarray[DTYPE_t, ndim=2] outCorrs = np.corrcoef(outMtx)
+        double errMoms = 1e50, errCorrs = 1e50 
+    
+    for idx in xrange(4):
+        outMoms[:, idx] = (outMtx**(idx+1)).mean(axis=1)
+    
+    errMoms = RMSE(outMoms, tgtMoms)
+    errCorrs = RMSE(outCorrs, tgtCorrs)
+    return errMoms, errCorrs
+
+
+cpdef RMSE(np.ndarray[DTYPE_t] srcArr, np.ndarray[DTYPE_t] tgtArr):
+    '''
+    srcArr, numpy.array, 1d or 2d
+    tgtArr, numpy.array, 1d or 2d
     '''
     assert srcArr.shape == tgtArr.shape
+    cdef double error = 1e50
     error = np.sqrt(((srcArr - tgtArr)**2).sum())
     return error 
 
 # @cython.boundscheck(False)
-def cubicTransform(np.ndarray[DTYPE_t, ndim=1] cubParams, 
+cpdef cubicTransform(np.ndarray[DTYPE_t, ndim=1] cubParams, 
                    np.ndarray[DTYPE_t, ndim=1] EY, 
                    np.ndarray[DTYPE_t, ndim=1] EX):
     '''
@@ -120,7 +163,12 @@ def cubicTransform(np.ndarray[DTYPE_t, ndim=1] cubParams,
     EY: 4 moments of target
     EX: 12 moments of samples
     '''
+    cdef:
+        double a, b, c, d
+        double v1, v2, v3, v4
+    
     a, b, c, d = cubParams
+    
     v1 = a + b*EX[0] + c*EX[1] + d*EX[2] - EY[0]
     
     v2 = ((d*d)*EX[5] + 2*c*d*EX[4] + (2*b*d+c*c)*EX[3] + 
@@ -145,88 +193,31 @@ def cubicTransform(np.ndarray[DTYPE_t, ndim=1] cubParams,
     return v1, v2, v3, v4
 
 
-cpdef  cubicSolve(np.ndarray[DTYPE_t, ndim=1] samples, 
-                           np.ndarray[DTYPE_t, ndim=1] probs,
-                           np.ndarray[DTYPE_t, ndim=1] origMoms):
+
+
+cpdef central2OrigMom(np.ndarray[DTYPE_t, ndim=2] centralMoms):
     '''
-    samples, size: n_scenario, sample of a r.v.
-    probs, size: n_scenario, probability of each scenario,
-    tgtMoms, size: 4, target original  moments
-    '''
-    cdef:
-        np.ndarray[DTYPE_t, ndim=1] sampleMoms = np.zeros(12)
-        n_scenario = samples.size
-        int idx
+    central moments to original moments
+    E[X] = samples.mean()
+    std**2 = var = E[X**2] - E[X]*E[X]
     
-    #計算samples的12階原始動差E[X]~E[X^12]
-    for idx in xrange(12):
-        sampleMoms[idx] = np.sum(probs * samples**(idx+1))
-    
-    
-    cdef:
-        # how often we increase the START_DEV 
-        int IncrDevStep = 50
-        
-        #L2-error on the target moments, at initial and final point
-        double error1,error2
-        
-        #infinite norm for the gradient
-        double ni
-        
-        #Hessian and inverse Hessian matrix
-        np.ndarray[DTYPE_t, ndim=2] Hessian = np.zeros((4,4))
-        np.ndarray[DTYPE_t, ndim=2] invHessian = np.zeros((4,4))
-        
-        #initial point of the solution
-        np.ndarray[DTYPE_t, ndim=1] X_init = np.array([0, 1, 0, 0])
-
-
-cdef void GradientHessian(np.ndarray[DTYPE_t, ndim=1] X)
+    scipy.stats.skew, scipy.stats.kurtosis公式如下：
+    m2 = np.mean((d - d.mean())**2)
+    m3 = np.mean((d - d.mean())**3)
+    m4 = np.mean((d - d.mean())**4)
+    skew =  m3/np.sqrt(m2)**3
+    kurt = m4/m2**2 -3
     '''
-    computes the gradient vector and the Hessian matrix of 
-    obj(X) at the current point X
-    X, size: 4
-    '''
-    cdef:
-        int i, j, k
-        np.ndarray[DTYPE_t, ndim=1] gradient = np.zeros(4)
-        np.ndarray[DTYPE_t, ndim=1] Hessian = np.zeros((4,4))
-        
+    cdef int n_rv = centralMoms.shape[0]
+    cdef np.ndarray[DTYPE_t, ndim=2] origMoms = np.empty((n_rv, 4))
+    
+    origMoms[:, 0] = centralMoms[:, 0]
+    origMoms[:, 1] = centralMoms[:, 1] ** 2  + centralMoms[:, 0]**2 
+    origMoms[:, 2] = (centralMoms[:, 2]*centralMoms[:, 1]**3+
+                      centralMoms[:, 0]**3+3*centralMoms[:, 0]*centralMoms[:, 1]**2)
+    origMoms[:, 3] = ((centralMoms[:, 3] + 3) * centralMoms[:, 1]**4  - centralMoms[:, 0]**4 + 
+                   4*centralMoms[:, 0]**4 - 6*centralMoms[:, 0]**2*origMoms[:, 1] + 4*centralMoms[:, 0]*origMoms[:, 2])  
 
-#     //first row initialisation
-#     for(i=0;i<7;i++){
-#         moment[0][i]=InMom[i];
-#     }
-# 
-#     //initilisation of the other rows
-# 
-#     momF1(moment[1],xk);
-#     momF2(moment[2],xk);
-#     momF3(moment[3],xk);
-#     moment[4][0]=momF4(xk);
+    return origMoms
 
-    #Gradient at point X
-    for(i=0;i<N;i++){
-        for(j=0;j<N;j++){
-        c[i]+=2*(j+1)*moment[j][i]*(moment[j+1][0]-TgMom[j]);
-        }
-    }
-
-    #hessian - no initialization needed, done for k=0
-    for(i=0;i<N;i++){
-        for (j=0; j<=i; j++){
-            // updated on 2010-06-17 - gcc complained about indexing error for k=0
-            Q[i][j] = 2*(moment[0][i]*moment[0][j]); // version for k=0
-            for (k=1; k<N; k++){
-                Q[i][j] += 2*(k+1)*((k+1)*moment[k][i]*moment[k][j]
-                                    +k*moment[k-1][i+j]*(moment[k+1][0]-TgMom[k]));
-            }
-            //symetrisation of the Hessian
-            if (j<i) {
-                Q[j][i]=Q[i][j];
-            }
-
-        }
-    }
-}
         
