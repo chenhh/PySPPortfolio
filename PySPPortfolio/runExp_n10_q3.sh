a8f5d26dc522f454db20c46146381f73fa49ff14
chenhh <chen1116@gmail.com>
2014-05-07 17:52:54 +0800

    exp scripts

---
+++ PySPPortfolio/runExp_n10_q3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for alpha in 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95 0.99
+do
+    for p in 100 110 120
+    do
+        python fixedSymbolSPPortfolio.py -n 10 -p $p -a $alpha  -m 200501 -f
+    done
+done
