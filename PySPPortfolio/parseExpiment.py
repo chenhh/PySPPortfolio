e193267527381ab736d24d22dee3850dec0e7b20
chenhh <chen1116@gmail.com>
2014-05-19 22:37:08 +0800

    parse

--- PySPPortfolio/parseExpiment.py
+++ PySPPortfolio/parseExpiment.py
@@ -95,7 +95,7 @@ def parseFixedSymbolResults():
 
 
 def parseDynamicSymbolResults(n_rv=50):
-    n_stocks = (5, 10, 15, 20)
+    n_stocks = (50,)
     hist_periods = range(70, 130, 10)
     n_scenario = 200
     alphas = ("0.5", "0.55", "0.6", "0.65", "0.7", 
@@ -170,5 +170,5 @@ def parseDynamicSymbolResults(n_rv=50):
 if __name__ == '__main__':
 #     readWealthCSV()
     parseFixedSymbolResults()
-#     parseDynamicSymbolResults()
+    parseDynamicSymbolResults()
    
