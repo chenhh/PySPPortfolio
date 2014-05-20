cdb990c147163c7cbc306f203915427cfa098233
chenhh <chen1116@gmail.com>
2014-05-21 07:09:19 +0800

    script

--- PySPPortfolio/runDymExp_4.sh
+++ PySPPortfolio/runDymExp_4.sh
@@ -2,7 +2,7 @@
 ns=$1
 for alpha in 0.75 0.8 0.85 0.9 0.95
 do 
-	for p in 70 80
+	for p in 70 80 90
 	do
 	   python dynamicSymbolSPPortfolio.py -n $ns -p $p -a $alpha  -f
    	done
--- PySPPortfolio/runDymExp_5.sh
+++ PySPPortfolio/runDymExp_5.sh
@@ -2,7 +2,7 @@
 ns=$1
 for alpha in 0.75 0.8 0.85 0.9 0.95
 do 
-	for p in 90 100
+	for p in  100 110 120
 	do
 	   python dynamicSymbolSPPortfolio.py -n $ns -p $p -a $alpha  -f
    	done
