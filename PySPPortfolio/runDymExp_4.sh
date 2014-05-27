effc042e0a6fb90fbc9bcc6f7e22039d888cd29d
chenhh <chenhh@x220>
2014-05-27 20:04:58 +0800

    n50

---
+++ PySPPortfolio/runDymExp_4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ns=$1
+for alpha in  0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.85 0.9 0.95
+do 
+	for p in 110 120
+	do
+	   python dynamicSymbolSPPortfolio.py -n $ns -p $p -a $alpha  -f
+   	done
+done
