d4a463d40744bebac6dc42f96edf3d9f0026d9ca
chenhh <chenhh@x220>
2014-05-28 20:16:45 +0800

    exp

--- PySPPortfolio/runExp_n15.sh
+++ PySPPortfolio/runExp_n15.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 for alpha in 0.8 0.85 0.9 0.95 0.99
 do
-    for p in 70 80 90 100 110 120
-    do
-        python fixedSymbolSPPortfolio.py -n 15 -p $p -a $alpha  -m 200501 -f
-    done
+    python fixedSymbolSPPortfolio.py -n 15 -p 50 -a $alpha  -m 200501 -f
+ 
 done
+
+for alpha in 0.85 0.9 0.95 0.99
+do
+    python fixedSymbolSPPortfolio.py -n 15 -p 60 -a $alpha  -m 200501 -f
+ 
+done
+
+ python fixedSymbolSPPortfolio.py -n 15 -p 120 -a 0.75  -m 200501 -f
