43ac7501b25f2e8366484a8467e2989391d46baa
chenhh <chen1116@gmail.com>
2014-05-29 20:58:38 +0800

    iparsewq

--- PySPPortfolio/parseExpiment.py
+++ PySPPortfolio/parseExpiment.py
@@ -52,8 +52,8 @@ def readWealthCSV():
 
 
 def parseFixedSymbolResults():
-#     n_rvs = range(5, 55, 5)
-    n_rvs = [50,]
+    n_rvs = range(5, 55, 5)
+#    n_rvs = [50,]
     hist_periods = range(50, 130, 10)
     alphas = ("0.5", "0.55", "0.6", "0.65", "0.7", 
               "0.75", "0.8", "0.85", "0.9", "0.95", "0.99")
