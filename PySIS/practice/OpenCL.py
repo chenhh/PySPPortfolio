6eeb77b20e2ed344acac2e7b51585e33efadd434
chenhh <chen1116@gmail.com>
2014-06-29 10:18:35 +0800

    opencl

--- PySIS/practice/OpenCL.py
+++ PySIS/practice/OpenCL.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 
+import time
 import numpy as np
 import pyopencl as cl
 
-a_np = np.random.rand(50000).astype(np.float32)
-b_np = np.random.rand(50000).astype(np.float32)
+n_sample = 50000
+a_np = np.random.rand(n_sample).astype(np.float32)
+b_np = np.random.rand(n_sample).astype(np.float32)
 
+t1 = time.time()
 ctx = cl.create_some_context()
 queue = cl.CommandQueue(ctx)
-
-mf = cl.mem_flags
-a_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=a_np)
-b_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=b_np)
 
 prg = cl.Program(ctx, """
 __kernel void sum(__global const float *a_g, __global const float *b_g, __global float *res_g) {
@@ -19,13 +18,21 @@ __kernel void sum(__global const float *a_g, __global const float *b_g, __global
   res_g[gid] = a_g[gid] + b_g[gid];
 }
 """).build()
+print "build time %f secs"%(time.time()-t1)
 
+t2 = time.time()
+mf = cl.mem_flags
+a_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=a_np)
+b_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=b_np)
 res_g = cl.Buffer(ctx, mf.WRITE_ONLY, a_np.nbytes)
 prg.sum(queue, a_np.shape, None, a_g, b_g, res_g)
 
 res_np = np.empty_like(a_np)
 cl.enqueue_copy(queue, res_np, res_g)
+print "compute time %f secs"%(time.time()-t2)
 
 # Check on CPU with Numpy:
+t3 = time.time()
 print(res_np - (a_np + b_np))
+print "cpu time %f secs"%(time.time()-t3)
 print(np.linalg.norm(res_np - (a_np + b_np)))
