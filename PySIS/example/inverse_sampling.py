bd39425c3948bf17834a37ff3961617b8bbe56e3
chenhh <chen1116@gmail.com>
2014-10-17 19:01:13 +0800

    inverse sampling example2

--- PySIS/example/inverse_sampling.py
+++ PySIS/example/inverse_sampling.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 '''
 .. codeauthor:: Hung-Hsin Chen <chenhh@par.cse.nsysu.edu.tw>
-如果target pdf的inverse analytic form已知，可用inverse cdf抽出樣本
+如果target pdf的inverse cdf analytic form已知，可用inverse cdf抽出樣本,
+抽出的樣本分佈即為target pdf
 '''
 __author__ = 'Hung-Hsin Chen'
 
@@ -29,5 +30,28 @@ def inverse_sampling(n_sample=10000):
     plt.plot(x, y, 'r',  linewidth=1.5)
     plt.show()
 
+
+def inverse_sampling2(exp_lambda, n_sample=10000):
+    '''
+    target pdf: X~Exp(lambda) = lambda* e**(−lambda*x)
+    its cdf F(x) = 1- e**(-lambda*x)
+    inverse cdf G(u = -1/lambda* log(1-u)
+    '''
+    #sample
+    pts = np.random.rand(n_sample)
+    samples = -np.log(1-pts)/exp_lambda
+
+    #function
+    x = np.linspace(0, 10, n_sample)
+    y = exp_lambda * np.exp(-exp_lambda*x)
+
+    n, bins, patches = plt.hist(samples, bins=100, normed=True)
+    # print n, bins, patches
+    plt.setp(patches, 'facecolor', 'g')
+    plt.title('X ~ Exp(%s)'%(exp_lambda))
+    plt.plot(x, y, 'r',  linewidth=1.5)
+    plt.show()
+
 if __name__ == '__main__':
-    inverse_sampling()
+    # inverse_sampling()
+    inverse_sampling2(2)
