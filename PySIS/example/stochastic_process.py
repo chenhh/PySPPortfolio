dab2ad4b451515d35fdd83eb8b39d71a357b5856
chenhh <chen1116@gmail.com>
2014-10-17 21:23:58 +0800

    importance sampling example

---
+++ PySIS/example/stochastic_process.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+'''
+.. codeauthor:: Hung-Hsin Chen <chenhh@par.cse.nsysu.edu.tw>
+'''
+__author__ = 'Hung-Hsin Chen'
+
+
+def homogeneous_Poisson(p_lambda=2, n_sample=10000):
+    '''
+    X(t) is homogeneous Poisson process if
+    1. X(t)有獨立增量性
+    2. P(X(s+t)-X(s)=n) = (lambda*t)**n/n!*e**(-lambda*t)
+    '''
+
+if __name__ == '__main__':
+    pass
