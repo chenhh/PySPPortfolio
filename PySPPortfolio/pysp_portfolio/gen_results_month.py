359d8b9692269802e0a00336e741a6596e1ce858
chenhh <chenhh@x220>
2016-04-23 02:19:12 +0800

    update multistage cvar event programming

--- PySPPortfolio/pysp_portfolio/gen_results_month.py
+++ PySPPortfolio/pysp_portfolio/gen_results_month.py
@@ -82,6 +82,22 @@ def all_experiment_parameters(prob_type, max_scenario_cnts):
                        pair[0], pair[1]))
             all_params.append((10, 90, 200, "unbiased", cnt, "0.50",
                        pair[0], pair[1]))
+            all_params.append((15, 100, 200, "unbiased", cnt, "0.65",
+                       pair[0], pair[1]))
+            all_params.append((20, 110, 200, "unbiased", cnt, "0.60",
+                       pair[0], pair[1]))
+            all_params.append((25, 120, 200, "unbiased", cnt, "0.55",
+                       pair[0], pair[1]))
+            all_params.append((30, 190, 200, "unbiased", cnt, "0.70",
+                       pair[0], pair[1]))
+            # all_params.append((35, 120, 200, "unbiased", cnt, "0.55",
+            #            pair[0], pair[1]))
+            # all_params.append((40, 100, 200, "unbiased", cnt, "0.50",
+            #            pair[0], pair[1]))
+            # all_params.append((45, 120, 200, "unbiased", cnt, "0.55",
+            #            pair[0], pair[1]))
+            # all_params.append((50, 120, 200, "unbiased", cnt, "0.55",
+            #            pair[0], pair[1]))
     return set(all_params)
 
 
